76f6eb0e9a0a7c6874cd643631b5eb7dab47df41
Mirajul Islam <237963166+mirajulbuilds@users.noreply.github.com>
2026-08-13 20:55:09 +0300

    fix: include react core in vendor chunk

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => ({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('react-dom') || id.includes('react-router'))
+            if (id.includes('/react/') || id.includes('react-dom') || id.includes('react-router'))
               return 'react-vendor';
             if (id.includes('lucide-react'))
               return 'icons';
